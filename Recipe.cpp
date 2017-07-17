977a14b143a9e556aa3e6e96d24462bd397a49be
gregjhansell97 <gregjhansell@gmail.com>
2017-07-17 18:46:53 -0400

    Added a semi naive algorithm for finding all recipes that can be made with given ingredients. It may or may not compile.

--- Recipe.cpp
+++ Recipe.cpp
@@ -9,6 +9,8 @@ Recipe::Recipe(const string& bn, string pn, const string& n){
   book_name = bn;
   page_number = pn;
   name = n;
+  occurence = 0;
+  match_count = 0;
 }
 
 void Recipe::add_ingredient(const string& n, const string& m){
@@ -30,4 +32,23 @@ void Recipe::write_to_file(ofstream& ofs) const{
   ofs << name << endl;
   ingredients.write_to_file(ofs);
   ofs << "----------" << endl;
+}
+
+vector<Recipe*> find_recipes(vector<string> &s){
+  static unsigned int occurence = 0;
+  vector<Recipe*> matched_recipes;
+  for(unsigned int i = 0; i < s.size(); i++){
+    list<Recipe*> r = Recipe::recipes[i];
+  	list<Recipe*>::iterator itr = r.begin();
+  	for(; itr != r.end(); itr++){
+      if((*itr)->occurence != occurence){
+        (*itr)->occurence = occurence;
+        (*itr)->match_count = 0;
+      } else if((*itr)->match_count_at_max){
+        matched_recipes.push_back(itr);
+      }
+      (*itr)->match_count++;
+  	}
+  }
+  occurence++;
 }
--- Recipe.h
+++ Recipe.h
@@ -16,7 +16,8 @@ using namespace std;
 class Recipe{
 public:
   static unordered_map<string, list< Recipe* > >  recipes; //recipes hashed to ingredients
-
+  unsigned int occurence = 0;
+  unsigned int match_count = 0;
   //bn = book name  pn = page number  n = name
   Recipe(const string& bn = "", string pn = "", const string& n = "");
 
@@ -25,7 +26,7 @@ public:
   string get_page_number() const{ return page_number;}
   string get_name() const{ return name; }
   string get_key() const{ return name + page_number + book_name; } //acts as the objects signature, that makes it easy to alphabetize
-
+  bool match_count_at_max const{ return match_count == ingredients.size(); }
   //setters
   //Add ingredient (n = name, m = measurement)
   void add_ingredient(const string& n, const string& m);
@@ -40,8 +41,9 @@ private:
   string book_name;
   string page_number;
   string name;
-
   Ingredient_Collection ingredients;
 };
+
+vector<Recipe> find_recipes(vector<string> &s);
 
 #endif
