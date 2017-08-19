03c25d96c04e676340a977ef5463f264a7d0ee9d
gregjhansell97 <gregjhansell@gmail.com>
2017-08-19 10:16:52 -0400

    Ingredient class is no longer connected to the measurment class, instead the two will be connected in an ingredient component class (potentially)

--- Ingredient.cpp
+++ Ingredient.cpp
@@ -1,14 +1,25 @@
 #include "Ingredient.h"
 
+
+bool Ingredient::locked = true;
+map<string, Ingredient*> Ingredient::ingredients;
+
+
+
+Ingredient& Ingredient::create_ingredient(string nm){
+  locked = false;
+  Ingredient* i;
+  i = ingredients[nm];
+  if(!i){
+    i = ingredients[nm] = new Ingredient;
+  }
+  return *i;
+}
+
+
 Ingredient::Ingredient(){
+  assert(!locked); //ensures that an ingredient object is only made through the factory function
   name = "";
-}
-
-Ingredient::Ingredient(const string& n, const string& m){
-  name = n;
-  measurement = m;
-}
-
-void Ingredient::print_data() const{
-  cout << name << " : " << measurement.get_name() << endl;
+  cout << "object created" << endl;
+  locked = true;
 }
--- Ingredient.h
+++ Ingredient.h
@@ -1,33 +1,39 @@
-#include "Measurement.h"
-#include <iostream>
 #include <string>
+#include <ostream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <assert.h>
 
 using namespace std;
-
+//need to add this comment to repush
 #ifndef Ingredient_h
 #define Ingredient_h
 
+enum Flavor{ SALTY, SWEET, SAVORY, SOUR, BITTER, SPICY };
+
 class Ingredient{
 public:
-  //constructors
-  //n = name, m = measurement
   Ingredient();
-  Ingredient(const string& n, const string& m);
+  Ingredient(string nm, )
 
   //getters
   string get_name() const{ return name; }
-  string get_measurement() const{ return measurement.get_name(); }
+  vector<Flavor> get_taste() const{ return taste; }
 
   //setters
-  void set_name(const string& n){ name = n; }
-  void set_measurement(const string& m){ measurement = m; }
 
-  //other
-  void print_data() const;
+  //factory function
+  static Ingredient& create_ingredient(string nm);
+
 
 private:
-  string name; //the name of the ingredient
-  Measurement measurement; //the how much of the ingredient
+  //Variables
+  string name; //like broth, pasta
+  vector<Flavor> taste; //SALTY, SWEET, SAVORY, SOUR, BITTER, SPICY
+  static bool locked; //prevents an object from being made unless it's from the static factory
+  static map<string, Ingredient*> ingredients;
 };
 
 #endif
